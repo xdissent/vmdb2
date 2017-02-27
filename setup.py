9d606a4e8c4ffff1dc3b6245c59abdccbc4da2f7
Lars Wirzenius <liw@liw.fi>
2017-02-27 19:44:16 +0200

    Add docs building to setup.py

--- setup.py
+++ setup.py
@@ -16,17 +16,62 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from distutils.core import setup, Extension
+from distutils.cmd import Command
+from distutils.command.build import build
+from distutils.command.clean import clean
 import glob
+
+import cliapp
 
 import vmdb
 
-setup(name='FIXME',
-      version=vmdb.__version__,
-      description='create disk image with Debian installed',
-      author='Lars Wirzenius',
-      author_email='liw@liw.fi',
-      url='http://liw.fi/vmdebootstrap/',
-      scripts=['vmdb2'],
-      packages=['vmdb'],
-      data_files=[('share/man/man1', glob.glob('*.1'))],
-     )
+
+class Build(build):
+
+    def run(self):
+        build.run(self)
+        self.build_manpage('obnam', '')
+        self.format_yarns()
+
+    def build_manpage(self, program, lang):
+        print 'building manpage for %s (lang=%s)' % (program, lang)
+        self.generate_troff(program, lang)
+        self.format_man_as_txt(program)
+
+    def generate_troff(self, program, lang):
+        with open('%s.1%s' % (program, lang), 'w') as f:
+            cliapp.runcmd(
+                ['python', program,
+                 '--generate-manpage=%s.1%s.in' % (program, lang),
+                 '--output=%s.1' % program],
+                stdout=f)
+
+    def format_man_as_txt(self, program):
+        env = dict(os.environ)
+        env['MANWIDTH'] = '80'
+        with open('%s.1.txt' % program, 'w') as f:
+            cliapp.runcmd(
+                ['man', '-l', '%s.1' % program],
+                ['col', '-b'],
+                stdout=f,
+                env=env)
+
+    def format_yarns(self):
+        print 'building yarns'
+        runcmd(['make', '-C', 'yarns'])
+
+
+setup(
+    name='FIXME',
+    version=vmdb.__version__,
+    description='create disk image with Debian installed',
+    author='Lars Wirzenius',
+    author_email='liw@liw.fi',
+    url='http://liw.fi/vmdebootstrap/',
+    scripts=['vmdb2'],
+    packages=['vmdb'],
+    data_files=[('share/man/man1', glob.glob('*.1'))],
+    cmdclass={
+        'build': Build,
+    },
+)
