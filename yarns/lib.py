90426d623ef7a49e748214700a327b5822b1f73b
Lars Wirzenius <liw@liw.fi>
2017-04-14 15:43:33 +0300

    Oops, drop the only use of YarnHelper

--- yarns/lib.py
+++ yarns/lib.py
@@ -4,11 +4,7 @@ import sys
 import cliapp
 from yarnutils import *
 
-import yarnhelper
-
 srcdir = os.environ['SRCDIR']
 datadir = os.environ['DATADIR']
 
 vars = Variables(datadir)
-
-helper = yarnhelper.YarnHelper()
