c4536cc2b065e330de33fe8524c37216adc0398e
Lars Wirzenius <liw@liw.fi>
2017-10-07 14:48:20 +0300

    Refactor: make use of vmdb.Spec in the main application
    This involves a change where step expansion is only done at the
    beginning. This doesn't currently matter, but in principle it does. We
    use attributes of the vmdb.State object as template variables, and if
    one step sets a variable, a later step could use it in a template. If
    this ever happens, we'll need to change things so that it works again.
    Currently nothing does that, and I'm reluctant to write that code
    until it's actually needed. YAGNI and all that.

--- vmdb/app.py
+++ vmdb/app.py
@@ -20,8 +20,6 @@ import logging
 import sys
 
 import cliapp
-import jinja2
-import yaml
 
 import vmdb
 
@@ -48,13 +46,14 @@ class Vmdb2(cliapp.Application):
         vmdb.set_verbose_progress(self.settings['verbose'])
 
         spec = self.load_spec_file(args[0])
+        state = vmdb.State()
+        params = self.create_template_vars(state)
+        steps = spec.get_steps(params)
 
-        steps = spec['steps']
         # Check that we have step runners for each step
         for step in steps:
             self.step_runners.find(step)
 
-        state = vmdb.State()
         steps_taken, core_meltdown = self.run_steps(steps, state)
         if core_meltdown:
             vmdb.progress('Something went wrong, cleaning up!')
@@ -68,8 +67,9 @@ class Vmdb2(cliapp.Application):
 
     def load_spec_file(self, filename):
         vmdb.progress('Load spec file {}'.format(filename))
-        with open(filename) as f:
-            return yaml.safe_load(f)
+        spec = vmdb.Spec()
+        spec.load_file(filename)
+        return spec
 
     def run_steps(self, steps, state):
         return self.run_steps_helper(
@@ -87,13 +87,12 @@ class Vmdb2(cliapp.Application):
             try:
                 logging.info(msg, step)
                 steps_taken.append(step)
-                expanded_step = self.expand_step_spec(step, state)
                 runner = self.step_runners.find(step)
                 if runner.skip(step, self.settings, state):
                     logging.info('Skipping as requested')
                 else:
                     method = getattr(runner, method_name)
-                    method(expanded_step, self.settings, state)
+                    method(step, self.settings, state)
             except BaseException as e:
                 vmdb.error(str(e))
                 core_meltdown = True
@@ -101,20 +100,6 @@ class Vmdb2(cliapp.Application):
                     break
 
         return steps_taken, core_meltdown
-
-    def format_step(self, runner, step):
-        return str(step)
-
-    def expand_step_spec(self, step, state):
-        expanded = {}
-        for key in step:
-            expanded[key] = self.expand_jinja2(step[key], state)
-        return expanded
-
-    def expand_jinja2(self, value, state):
-        template = jinja2.Template(value)
-        vars = self.create_template_vars(state)
-        return template.render(**vars)
 
     def create_template_vars(self, state):
         vars = dict()
